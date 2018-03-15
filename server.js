7d4e7291945f1f302fec30b0ae3de37d16405d61
David Nyman <davenyman@gmail.com>
2018-03-15 14:14:11 -0400

    Change background
    I changed the background color

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
