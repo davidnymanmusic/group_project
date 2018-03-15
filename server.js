8f69f2bc6457c64d91a028d75ee57de6589e38bc
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-03-15 13:30:29 -0400

    modified

--- server.js
+++ server.js
@@ -3,21 +3,14 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.render('site/index', {
-		title: 'GROUP PROJECT WITH D&D'
-	});
+  res.send('Group project with david');
 });
-app.get('/about', (req, res) => {
-  res.send("This is what we're all about");
-});
-
-app.get('/about', (req, res) => {
-  res.send("what we're all about");
-});
-
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Running on ${PORT}`);
 });
+
+// eslint-disable-next-line no-console
