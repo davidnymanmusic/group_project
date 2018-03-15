ae529376b7687a26bd2c13534821bf0a4ef28f26
David Nyman <davenyman@gmail.com>
2018-03-15 13:33:40 -0400

    Add view
    Added set view engine

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   res.render('site/index', {
 		title: 'GROUP PROJECT WITH D&D'
